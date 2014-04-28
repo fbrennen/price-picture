7ec5d722afc2e7129c0028b150a8e76dd6e6d237
Forrest <fbrennen@localhost.localdomain>
2014-04-28 19:47:37 +0100

    Corrected reduce so it can be run multiple times per key.

--- coarsify_postcodes.py
+++ coarsify_postcodes.py
@@ -5,24 +5,27 @@ from bson import Code
 
 # We've got very detailed postcode data, but are more interested in a larger
 # scale for now, so we'll aggregate everything under the first bit of the
-# postcode.
+# postcode, and average the coordinates together.
 
 def coarsify(input_collection, output_collection_name):
     # TODO: This map won't work properly for a few postcodes, such as those
-    # with single-letter initial designators. Fix that sucker.
+    # with single-letter initial designators. Will work for OX* for now tho!
     themap = Code("function () {"
                   "  emit(this.postcode.substring(0,4).trim(),"
-                  "       { lat:this.lat, long:this.long });"
+                  "       { lat:this.lat, long:this.long, count:1 });"
                   "}")
     thereduce = Code("function(key, values) {"
                      "  lattotal = 0;"
                      "  longtotal = 0;"
+                     "  count = 0;" # So we can reduce multiple times per key
                      "  for(i in values) {"
-                     "    lattotal += values[i].lat;"
-                     "    longtotal += values[i].long;"
+                     "    lattotal += values[i].lat * values[i].count;"
+                     "    longtotal += values[i].long * values[i].count;"
+                     "    count += values[i].count;"
                      "  }"
-                     "  return { 'lat': lattotal / values.length,"
-                     "           'long': longtotal / values.length };"
+                     "  return { 'lat': lattotal / count,"
+                     "           'long': longtotal / count,"
+                     "           'count': count };"
                      "}")
     input_collection.map_reduce(themap, thereduce, output_collection_name)
     
