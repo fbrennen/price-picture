51d3425b86fa6abc4b8356a7cd25ee84b3fd02fd
Forrest Brennen <fbrennen@users.noreply.github.com>
2014-05-12 18:15:30 +0100

    Added licensing.

--- db_schemas.py
+++ db_schemas.py
@@ -1,5 +1,24 @@
 #! /usr/bin/python
 
+""" Copyright 2014 Forrest Brennen
+
+    This file is part of price_picture.
+
+    price_picture is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    price_picture is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with price_picture.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 """
 db_schemas.py
 
